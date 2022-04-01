d4c9669b37754db20b34df305598766c5129f4b4
yellrim <93649193+yellrim@users.noreply.github.com>
2022-04-01 16:33:02 +0900

    Add files via upload

--- js/myscript.js
+++ js/myscript.js
@@ -16,3 +16,32 @@
                 nowSlide++;
             }
         },3000);
+
+        // 모달
+        let modalN = document.querySelector('.modal-notice');
+        let modalTit = document.querySelector('.modal-tit');
+        let modalTxt = document.querySelector('.modal-txt');
+        let modalTitle = document.querySelectorAll('.notice-content .title');
+        let modalCon = document.querySelectorAll('.notice-content .content');
+        let btnCloseN = document.querySelector('.modal-notice .modal-close');
+
+
+        for(let idx=0; idx<modalTitle.length; idx++){
+            
+            modalTitle[idx].addEventListener('click',function(e){
+                noticeBtn('block');
+                modalTit.innerHTML = this.innerHTML;
+                modalTxt.innerHTML = modalCon[idx].innerHTML;
+
+                e.preventDefault();
+            })
+            
+        }
+
+        btnCloseN.addEventListener('click',function(){
+            noticeBtn('none');
+        })
+
+        function noticeBtn(e){
+            modalN.style.display = e;
+        }
